8606ae8fda0330111716f5c4b607a0569a23bfb9
Time-Coder <binghui.wang@foxmail.com>
2025-06-21 18:52:52 +0800

    fix upload error

--- upload.py
+++ upload.py
@@ -1,4 +1,4 @@
 import subprocess
 import sys
 
-subprocess.call([sys.executable, "-m", "twine", "upload", "dist/tree_sitter_glsl37-*.tar.gz", "dist/tree_sitter_glsl37-*.whl", "--verbose"])
+subprocess.call([sys.executable, "-m", "twine", "upload", "dist/tree-sitter-glsl37-*.tar.gz", "dist/tree_sitter_glsl37-*.whl", "--verbose"])
